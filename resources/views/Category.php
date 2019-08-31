eeeca599c7eb5013f99ef6210df39d1c3180c318
Fajar Siddiqi Putu Saed <47381662+fajardiki@users.noreply.github.com>
2019-08-31 20:06:42 +0700

    Lumen

---
+++ resources/views/Category.php
@@ -0,0 +1,9 @@
+<?php 
+	namespace App; 
+	use IlluminateDatabaseEloquentModel; 
+
+	class Category extends Model {     
+		protected $fillable = ['name'];     
+		public $timestamps = false;     
+		public static $rules = ['name' => 'required']; 
+	}
