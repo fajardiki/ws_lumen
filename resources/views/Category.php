11945bb9b1bb87e8ebf92e2a0fe27ea6397baf3b
Fajar Siddiqi Putu Saed <47381662+fajardiki@users.noreply.github.com>
2019-09-06 18:31:55 +0700

    Nyobak lagi ni
    Sesungguhnya menyerah bukan pilihan sedang mencoba adalah permainan

--- resources/views/Category.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	namespace App; 
-	use IlluminateDatabaseEloquentModel; 
-
-	class Category extends Model {     
-		protected $fillable = ['name'];     
-		public $timestamps = false;     
-		public static $rules = ['name' => 'required']; 
-	}
